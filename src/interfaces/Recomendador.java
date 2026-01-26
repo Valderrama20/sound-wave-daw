872fc12d08f2b8fd5aa34dc004c07137cbd960a6
valderrama20 <jose23122009@gmail.com>
2026-01-26 21:15:05 +0100

    ajustar tipo de retorno en las interfaz Recomendador

--- src/interfaces/Recomendador.java
+++ src/interfaces/Recomendador.java
@@ -1,8 +1,8 @@
 package interfaces;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public interface Recomendador {
-    List<Contenido> recomendar(Usuario usuario);
-    List<Contenido> obtenerSimilares(Contenido contenido);
+    ArrayList<Contenido> recomendar(Usuario usuario);
+    ArrayList<Contenido> obtenerSimilares(Contenido contenido);
 }
