b8d2c225f83ffc5a8be83669220103d52768e679
valderrama20 <jose23122009@gmail.com>
2026-02-01 17:13:16 +0100

    documentar interfaces

--- src/interfaces/Descargable.java
+++ src/interfaces/Descargable.java
@@ -1,7 +1,28 @@
 package interfaces;
 
+/**
+ * Contrato para contenido descargable para uso offline
+ */
 public interface Descargable {
+
+    /**
+     * Descarga el contenido. Debe fallar si supera límites o ya está descargado
+     *
+     * @return true si la descarga se realizó correctamente, false en caso contrario
+     */
     boolean descargable();
+
+    /**
+     * Elimina la descarga existente
+     *
+     * @return true si la descarga fue eliminada correctamente, false si no existía o falló
+     */
     boolean eliminarDescarga();
+
+    /**
+     * Devuelve el espacio (aproximado) requerido para almacenar la descarga (en MB)
+     *
+     * @return espacio requerido para la descarga en MB
+     */
     int espacioRequerido();
 }
--- src/interfaces/Recomendador.java
+++ src/interfaces/Recomendador.java
@@ -5,7 +5,24 @@ import usuarios.Usuario;
 
 import java.util.ArrayList;
 
+/**
+ * Contrato del sistema de recomendaciones
+ */
 public interface Recomendador {
+
+    /**
+     * Genera recomendaciones personalizadas para el usuario
+     *
+     * @param usuario
+     * @return una lista de contenidos
+     */
     ArrayList<Contenido> recomendar(Usuario usuario);
+
+    /**
+     * Obtiene contenido similar a un contenido dado
+     *
+     * @param contenido
+     * @return una lista de contenido
+     */
     ArrayList<Contenido> obtenerSimilares(Contenido contenido);
 }
